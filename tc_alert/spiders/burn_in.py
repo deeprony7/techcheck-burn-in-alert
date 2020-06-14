39f5aa7c60522703c7ddfe3de534c87d788086e5
Shouvick Halder <deeprony7@gmail.com>
2020-06-13 21:00:50 -0400

    working script using scrapy splash

---
+++ tc_alert/spiders/burn_in.py
@@ -0,0 +1,91 @@
+'''
+Idea is to use scrapy splash to render the javascript page "https://techcheck.cengage.com/" and parse platform status for the burn-in systems. If there is a change of platform state then program will send out an email to team's group DL.
+1. render
+2. parse
+3. email
+'''
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy_splash import SplashRequest
+import smtplib
+import json
+import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def send_mail(message, title):
+        print("Sending mail...........")     
+        gmailUser = os.getenv('W_EMAIL')
+        gmailPassword = os.getenv('W_PASS')
+        recipient = os.getenv('W_EMAIL')    #   multiple recipients = 'xyz@smartshifttech.com,xxxxxx@smartshifttech.com'
+        msg = MIMEMultipart()
+        msg['From'] = gmailUser
+        msg['To'] = recipient
+        msg['Subject'] = title
+        msg.attach(MIMEText(message))
+        mailServer = smtplib.SMTP('smtp.gmail.com', 587)
+        mailServer.ehlo()
+        mailServer.starttls()
+        mailServer.ehlo()
+        mailServer.login(gmailUser, gmailPassword)
+        mailServer.sendmail(gmailUser, recipient.split(',') , msg.as_string())  #   multiple recipients
+        mailServer.close()
+        print("Mail sent")
+
+class BurnInSpider(scrapy.Spider):
+    name = 'burn_in'
+    allowed_domains = ['www.techcheck.cengage.com']
+    # perhaps modify to send_keys("<ENTER>") after entering password?
+    script = f'''
+        function main(splash, args)
+            splash:on_request(function(request)
+              request:set_header('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36')
+            end)
+            splash.private_mode_enabled = false
+            url = args.url
+            assert(splash:go(args.url))
+            assert(splash:wait(6))
+            username = assert(splash:select("#username-field"))
+            username:focus()
+            username:send_text("shalder")
+            assert(splash:wait(0.5))
+            passwd = assert(splash:select("#password-field"))
+            passwd:focus()
+            passwd:send_text("{os.getenv('TC_PASS')}")
+            assert(splash:wait(0.5))
+            btn = assert(splash:select("#login-btn > span.MuiButton-label"))    
+            btn:mouse_click()
+            assert(splash:wait(1))
+            home = assert(splash:select("#home-btn > span.MuiIconButton-label > span"))
+            home:mouse_click()
+            assert(splash:wait(2))
+            splash:set_viewport_full()
+            return {{
+                html = splash:html()
+                }}
+            end
+    '''
+
+    def start_requests(self):
+        yield SplashRequest(url="https://techcheck.cengage.com/login", callback=self.parse, endpoint='execute', args={
+            'lua_source': self.script
+        })
+    
+
+    def parse(self, response):
+        # read old data to compare state
+        with open('record_state.json', 'r') as fp:
+            old_state = json.load(fp)
+        dict = {}
+        for platform in response.xpath("//div[@class='MuiPaper-root MuiCard-root SplashPage_system_18pPS SplashPage_internal_1RvCv MuiPaper-elevation1 MuiPaper-rounded']"):
+            dict.update({platform.xpath(".//a/div/div[2]/p/text()").get():platform.xpath(".//div/div[1]/div[1]/text()").get()})  
+        # write new data
+        with open("record_state.json", "w") as write_data:
+                    json.dump(dict, write_data)
+        
+        platforms_monitored = ['Diet & Wellness Plus Health Check', 'ANZ HE Commerce - Search and Add to Cart', 'ANZ HE Commerce - SOLR Service', 'ANZ HE Commerce - Your Cart', 'Jira [only affects internal users]', 'eCommerce US - Search and Add to Cart (new 2020)']
+
+        for platform in platforms_monitored:
+            if (old_state[platform] != dict[platform]):
+                msg = f"{platform} platform status changed to {dict[platform]}"
+                send_mail(msg, msg)
---
+++ tc_alert/spiders/burn_in_selenium2.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+import json, os, smtplib
+import scrapy
+from scrapy.selector import Selector
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from scrapy_selenium import SeleniumRequest
+
+def send_mail(message, title):
+        print("Sending mail...........")     
+        gmailUser = os.getenv('W_EMAIL')
+        gmailPassword = os.getenv('W_PASS')
+        recipient = os.getenv('W_EMAIL')    #   multiple recipients = 'xyz@smartshifttech.com,xxxxxx@smartshifttech.com'
+        msg = MIMEMultipart()
+        msg['From'] = gmailUser
+        msg['To'] = recipient
+        msg['Subject'] = title
+        msg.attach(MIMEText(message))
+        mailServer = smtplib.SMTP('smtp.gmail.com', 587)
+        mailServer.ehlo()
+        mailServer.starttls()
+        mailServer.ehlo()
+        mailServer.login(gmailUser, gmailPassword)
+        mailServer.sendmail(gmailUser, recipient.split(',') , msg.as_string())  #   multiple recipients
+        mailServer.close()
+        print("Mail sent")
+
+class CoinSpiderSelenium(scrapy.Spider):
+    name = 'burn_in_selenium2'
+    # allowed_domains = ['www.techcheck.cengage.com']
+    # start_urls = [
+    #     'https://techcheck.cengage.com/login'
+    # ]
+
+    # def __init__(self):
+    #     chrome_options = Options()
+    #     chrome_options.add_argument('--headless')
+    #     chrome_path = '/home/shouvick/PycharmProjects/untitled/scrapy-projects/livecoin/chromedriver'
+
+    #     driver = webdriver.Chrome(executable_path=chrome_path, options=chrome_options)
+    #     driver.set_window_size(1920,5080)
+    #     driver.get("https://techcheck.cengage.com/login")
+    #     driver.implicitly_wait(15)
+    #     username = driver.find_element_by_css_selector("#username-field")
+    #     username.click()
+    #     username.send_keys("shalder")
+    #     passwd = driver.find_element_by_css_selector("#password-field")
+    #     passwd.click()
+    #     passwd.send_keys(f"{os.getenv('TC_PASS')}")
+    #     passwd.send_keys(Keys.RETURN)
+    #     driver.implicitly_wait(10)
+    #     driver.get("https://techcheck.cengage.com")
+    #     driver.implicitly_wait(20)
+
+    #     self.html = driver.page_source
+    #     driver.close()
+    def start_requests(self):
+        yield SeleniumRequest(
+            url='https://techcheck.cengage.com/login',
+            wait_time=5,
+            callback=self.parse
+        )
+    def parse(self, response):
+        driver = response.meta['driver']
+        username = driver.find_element_by_css_selector("#username-field")
+        username.click()
+        username.send_keys("shalder")
+        passwd = driver.find_element_by_css_selector("#password-field")
+        passwd.click()
+        passwd.send_keys(f"{os.getenv('TC_PASS')}")
+        passwd.send_keys(Keys.RETURN)
+        
+        
+        # response = Selector(text=self.html)
+        # read old data to compare state
+        # with open('record_state.json', 'r') as fp:
+        #     old_state = json.load(fp)
+
+        # dict = {}
+        # for platform in response.xpath("//div[@class='MuiPaper-root MuiCard-root SplashPage_system_18pPS SplashPage_internal_1RvCv MuiPaper-elevation1 MuiPaper-rounded']"):
+        #     dict.update({platform.xpath(".//a/div/div[2]/p/text()").get():platform.xpath(".//div/div[1]/div[1]/text()").get()}) 
+        # print(dict)
+        # # write new data
+        # with open("record_state.json", "w") as write_data:
+        #             json.dump(dict, write_data)
+        
+        # platforms_monitored = ['Diet & Wellness Plus Health Check', 'ANZ HE Commerce - SOLR Service', 'ANZ HE Commerce - Your Cart', 'Jira [only affects internal users]', 'eCommerce US - Search and Add to Cart (new 2020)']
+
+        # for platform in platforms_monitored:
+        #     if (old_state[platform] != dict[platform]):
+        #         msg = f"{platform} platform status changed to {dict[platform]}"
+        #         send_mail(msg, msg)
